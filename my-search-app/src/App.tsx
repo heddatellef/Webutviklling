27cdbeff9d946b0b3a886452c5e10cbfbe4603d9
vaarss <vaarss@stud.ntnu.no>
2020-10-14 12:37:13 +0200

    #1 Fixed some basic layout for search and navbar

--- my-search-app/src/App.tsx
+++ my-search-app/src/App.tsx
@@ -1,24 +1,37 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Button } from '@material-ui/core';
+import { BottomNavigationAction } from '@material-ui/core'; 
+import { BottomNavigation } from '@material-ui/core'; 
+import { Input } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import { Home, Favorite, AccessTime } from '@material-ui/icons';
+
+const useStyles = makeStyles({
+  root: {
+    color: 'white',
+  },
+});
 
 function App() {
   return (
-    <div className="App">
+    <div className="App"> 
+    <header className="top">
+      <BottomNavigation showLabels className="navbar">
+      <BottomNavigationAction label="Home" value="home" icon={<Home />} />
+      <BottomNavigationAction label="Favorites" value="favorites" icon={<Favorite />} />
+      <BottomNavigationAction label="Recent" value="recent" icon={<AccessTime />} />
+      
+    </BottomNavigation>
+    </header>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Input placeholder="Name" inputProps={{ 'aria-label': 'description' }} />
+        <Input placeholder="Club" inputProps={{ 'aria-label': 'description' }} />
+        <Input placeholder="Country" inputProps={{ 'aria-label': 'description' }} />
+        <Input placeholder="Other"  inputProps={{ 'aria-label': 'description' }} />
+        <Button color="primary">Search</Button>
       </header>
+      
     </div>
   );
 }
