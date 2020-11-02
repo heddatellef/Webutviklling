b55f73a2425c5528324abf2fc29acc15dbd4ceb1
vaarss <vaarss@stud.ntnu.no>
2020-11-02 16:20:52 +0100

    Made search function work by putting countrynames in interface in frontend (SearchCountryName.tsx)

--- my-search-app/src/App.tsx
+++ my-search-app/src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import "bootstrap/dist/css/bootstrap.min.css";
 import './App.css';
 import { Button } from '@material-ui/core';
 import { Input } from '@material-ui/core';
